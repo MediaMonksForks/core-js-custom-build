dc10d6206d83be48c88bf0830d994e6db43f1a3e
Floris Bernard <floris.bernard@gmail.com>
2019-07-23 20:18:33 +0200

    Initial commit

---
+++ modules/es.regexp.flags.js
@@ -0,0 +1,15 @@
+var regExpFlags = require('../internals/regexp-flags');
+
+// `RegExp.prototype.flags` getter
+// https://tc39.github.io/ecma262/#sec-get-regexp.prototype.flags
+if (
+  require('../internals/descriptors') && (
+    /./g.flags != 'g' ||
+    require('../internals/regexp-sticky-helpers').UNSUPPORTED_Y
+  )
+) {
+  require('../internals/object-define-property').f(RegExp.prototype, 'flags', {
+    configurable: true,
+    get: regExpFlags
+  });
+}
